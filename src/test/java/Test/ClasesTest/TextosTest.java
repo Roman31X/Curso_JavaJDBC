7644d1e7b90a86f9c7e7a0a60a02fea03f41e091
Roman31X <die.haa65@gmail.com>
2023-08-17 21:11:33 -0500

    Test1

--- src/test/java/Test/ClasesTest/TextosTest.java
+++ src/test/java/Test/ClasesTest/TextosTest.java
@@ -9,7 +9,23 @@ public class TextosTest {
 
         String cadena = Textos.reporte("Hola",3);
         System.out.println("cadena : " + cadena);
+        VerificaResultado(cadena,"HolaHolaHola");
+        /*if(cadena.equals("HolaHolaHola")){
+            System.out.println("Todo salio bien");
+        }else{
+            //System.out.println("jajaja Fallo como tu relación");
+            throw new RuntimeException("Error como tu relacion de de paso que");
+        }*/
          
+    }
+
+    private static void VerificaResultado(String cadena, String palabra) {
+        if(cadena.equals(palabra)){
+            System.out.println("Todo salio bien");
+        }else{
+            //System.out.println("jajaja Fallo como tu relación");
+            throw new RuntimeException("Error como tu relación");
+        }
     }
 
 }
