68d8f24652bf67182dfbea0cdf47041fb46f2e02
Roman31X <die.haa65@gmail.com>
2024-05-25 15:10:00 -0500

    Listado Correcto

--- src/main/java/zona_Fit/datos/ClienteDAO.java
+++ src/main/java/zona_Fit/datos/ClienteDAO.java
@@ -2,12 +2,41 @@ package zona_Fit.datos;
 
 import zona_Fit.dominio.Cliente;
 
-import java.util.List;
+import java.sql.*;
+import java.util.*;
+
+import static zona_Fit.conexion.Conexion.getConexion;
 
 public class ClienteDAO implements IClienteDAO{
     @Override
     public List<Cliente> listarClientes() {
-        return null;
+        List<Cliente> clientes = new ArrayList<>();
+        PreparedStatement ps;
+        ResultSet rs;
+        Connection con = getConexion();
+
+        var sql = "SELECT * FROM cliente ORDER BY id";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var cliente = new Cliente();
+                cliente.setId(rs.getInt("id"));
+                cliente.setNombre(rs.getString("nombre"));
+                cliente.setApellido(rs.getString("apellido"));
+                cliente.setMembresia(rs.getInt("membresia"));
+                clientes.add(cliente);
+            }
+        }catch (Exception e){
+            System.out.println("Error al listar clientes: "+e.getMessage());
+        }finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar conexión: "+e.getMessage());
+            }
+        }
+        return clientes;
     }
 
     @Override
@@ -28,5 +57,12 @@ public class ClienteDAO implements IClienteDAO{
     @Override
     public boolean eliminarCliente(Cliente cliente) {
         return false;
+    }
+
+    //PRUEBA EN LOCAL
+    public static void main(String[] args) {
+        var prueba = new ClienteDAO();
+        List<Cliente> usuarios = prueba.listarClientes();
+        usuarios.forEach(System.out::println);
     }
 }
